f60a25ede937e08e84b69b72e0c55c9160f394b8
Natchii59 <caron.nathan@hotmail.com>
2022-02-13 12:29:49 +0100

    Add simple users manager

--- start/src/users/users.controller.ts
+++ start/src/users/users.controller.ts
@@ -1,6 +1,27 @@
 // Generate with CLI : nest generate controller users
 
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  getUsers(): User[] {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string): User {
+    // TODO: Auto parse param id
+    return this.usersService.findById(Number(id));
+  }
+
+  @Post()
+  createUser(@Body() body: CreateUserDto): User {
+    return this.usersService.createUser(body);
+  }
+}
--- start/src/users/users.service.ts
+++ start/src/users/users.service.ts
@@ -1,6 +1,32 @@
 // Generate with CLI : nest generate controller users
 
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    { id: 0, name: 'Nathan' },
+    { id: 1, name: 'Léa' },
+  ];
+
+  findAll(): User[] {
+    return this.users;
+  }
+
+  findById(userId: number): User {
+    return this.users.find((user) => user.id === userId);
+  }
+
+  createUser(createUserDto: CreateUserDto): User {
+    const newUser = {
+      ...createUserDto,
+      id: Date.now(),
+    };
+
+    this.users.push(newUser);
+
+    return newUser;
+  }
+}
