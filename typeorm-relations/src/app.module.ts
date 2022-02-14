04ef48b92b6882d2c421c0402fec04622c7100a6
Natchii59 <caron.nathan@hotmail.com>
2022-02-14 18:14:34 +0100

    Initial Commit for typeorm-relations

---
+++ typeorm-relations/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      username: 'devuser',
+      database: 'nestjs_training',
+      entities: ['dist/src/**/*.entity.js'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
