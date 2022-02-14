2e917eb39ac5c824941f61839ec2fa771122dbcf
Natchii59 <caron.nathan@hotmail.com>
2022-02-14 18:05:08 +0100

    Create CRUD

--- typeorm/src/app.controller.ts
+++ typeorm/src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from './user.entity';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async seed(): Promise<User> {
+    const user = await this.appService.createUser('Nathan');
+    await this.appService.createPet('TouTou', user);
+
+    return await this.appService.updateUser(user.id, 'New name');
+  }
+
+  @Get('all')
+  all(): Promise<User[]> {
+    return this.appService.getAll();
+  }
+
+  @Delete('delete/:id')
+  delete(@Param('id', ParseIntPipe) id: number): Promise<User> {
+    return this.appService.deleteUser(id);
   }
 }
--- typeorm/src/app.service.ts
+++ typeorm/src/app.service.ts
@@ -1,7 +1,61 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { Pet } from './pet.entity';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+    @InjectRepository(Pet) private petsRepository: Repository<Pet>,
+  ) {}
+
+  getAll(): Promise<User[]> {
+    return this.usersRepository.find({
+      relations: ['pets'],
+    }); // SELECT * FROM user JOIN pets;
+  }
+
+  async getOneById(id: number): Promise<User> {
+    try {
+      return await this.usersRepository.findOne(id); // SELECT * FROM user WHERE id = '$id';
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  createUser(name: string): Promise<User> {
+    const newUser = this.usersRepository.create({
+      name,
+    });
+    // const newUser = new User();
+    // newUser.name = name;
+
+    return this.usersRepository.save(newUser); // INSERT
+  }
+
+  async updateUser(id: number, name: string): Promise<User> {
+    const user = await this.getOneById(id);
+
+    return this.usersRepository.save({
+      ...user,
+      name,
+    });
+  }
+
+  async deleteUser(id: number): Promise<User> {
+    const user = await this.getOneById(id);
+
+    return this.usersRepository.remove(user);
+  }
+
+  async createPet(name: string, owner: User): Promise<Pet> {
+    const newPet = this.petsRepository.create({ owner, name });
+
+    return this.petsRepository.save(newPet);
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
